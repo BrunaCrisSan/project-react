eb002aacb4b2613a836f369e47efbc464555f420
bruna <brunamant@gmail.com>
2025-11-25 16:13:06 -0300

    TaskCard

---
+++ src/components/TaskCard/TaskCard.jsx
@@ -0,0 +1,10 @@
+const TaskCard = ({task, onStatusChange}) => {
+  const getPriorityClass = (priority) => {
+    switch (priority){
+      case 'high': return 'priority-high';
+      case 'medium': return 'priority-medium';
+      case 'low': return 'priority-low';
+      default: return '';
+    }
+  }
+}
